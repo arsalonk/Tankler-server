21c214af62a306240ef767e26e453333f1a230a5
arsalon <arsalon47@gmail.com>
2018-06-08 10:36:56 -0400

    added to database

--- routes/database.js
+++ routes/database.js
@@ -11,6 +11,7 @@ router.use('/', passport.authenticate('jwt', { session: false, failWithError: tr
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
   Database.find({})
+    .sort('name')
     .then(results => {
       res.json(results);
     })
