6d1b2f42153e91539879e7c0ed3b5a045ab3214d
arsalon <arsalon47@gmail.com>
2018-06-08 12:02:57 -0400

    README created

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -2,14 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { DATABASE_URL } = require('../config');
 
-const task = require('../models/task');
-const parameter = require('../models/parameter');
 const database = require('../models/database')
-const tank = require('../models/tank')
-const livestock = require('../models/livestock')
-
-const seedTasks = require('../db/seed/tasks');
-const seedParameters = require('../db/seed/parameters')
 const seedDatabase = require('../db/seed/database')
 
 mongoose.connect(DATABASE_URL)
@@ -19,14 +12,8 @@ mongoose.connect(DATABASE_URL)
   })
   .then(() => {
     return Promise.all([
-      task.insertMany(seedTasks),
-      task.createIndexes(),
-      parameter.insertMany(seedParameters),
-      parameter.createIndexes(),
       database.insertMany(seedDatabase),
-      database.createIndexes(),
-      tank.createIndexes(),
-      livestock.createIndexes()
+      database.createIndexes()
     ]);
   })
   .then(() => mongoose.disconnect())
